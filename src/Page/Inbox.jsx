1d2d808cdb0f7fec48f336e1f156b9f4f51d9359
akd420 <ayanakd112@gmail.com>
2024-01-18 00:56:10 +0600

    added inbox part with fake data

--- src/Page/Inbox.jsx
+++ src/Page/Inbox.jsx
@@ -1,10 +1,40 @@
-
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import lott from "../assets/lott.json";
+import InboxCard from "../Components/InboxCard";
 const Inbox = () => {
-    return (
-        <div>
-            this is Inbox
+  const [emailData, setEmailData] = useState([]);
+  useEffect(() => {
+    fetch("./emailData.json")
+      .then((res) => res.json())
+      .then((data) => setEmailData(data));
+  }, []);
+  return (
+    <div className="max-w-screen-xl mx-auto my-12 px-4">
+      {/* if there are no emails yet show this  */}
+
+      {emailData.length > 0 && (
+        <div className="md:w-96 mx-auto">
+          <h1 className="text-center font-bold text-2xl">No messages yet</h1>
+          <Lottie animationData={lott} />
         </div>
-    );
+      )}
+
+      {/* if there are emails show this  */}
+      {emailData.length < 0 && (
+        <div>
+          <h1 className="text-4xl font-bold mb-5">
+            Inbox ({emailData.length})
+          </h1>
+          <div>
+            {emailData.map((data) => (
+              <InboxCard key={data._id} data={data}></InboxCard>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Inbox;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,24 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomeLayout from "../Layout/HomeLayout";
-import Inbox from "../Page/Inbox";
+import App from "../App";
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <HomeLayout></HomeLayout>,
+    children: [
+      {
         path: "/",
-        element:<HomeLayout></HomeLayout>,
-        children:[
-            {
-                path: "/",
-                element:<Inbox></Inbox>
-            },
-            {
-                path: "/contact",
-                element: <h1>this contact Us</h1>
-            },
-            {
-                path: "/about",
-                element:<h1>this About Us</h1>
-            },
-                ]
-    }
-])
+        element: <App></App>,
+      },
+      {
+        path: "/contact",
+        element: <h1>this contact Us</h1>,
+      },
+      {
+        path: "/about",
+        element: <h1>this About Us</h1>,
+      },
+    ],
+  },
+]);
