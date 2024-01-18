88ac3970a555c3ce872bcd2cf6f0a1e1cf6586d9
akd420 <ayanakd112@gmail.com>
2024-01-18 12:13:23 +0600

    Added pallabs inboxdetails part

--- src/Page/InboxDetails.jsx
+++ src/Page/InboxDetails.jsx
@@ -7,6 +7,7 @@ import { Link, useParams } from "react-router-dom";
 const InboxDetails = () => {
   const { id } = useParams();
   const [emailData, setEmailData] = useState([]);
+  const currentData = emailData[parseInt(id) - 1];
 
   useEffect(() => {
     fetch("/emailData.json")
@@ -15,33 +16,33 @@ const InboxDetails = () => {
   }, []);
 
   return (
-    <div className="pt-10 px-10">
+    <div className="pt-10 lg:px-10">
       <Link to="/">
         <button className="mb-3 flex items-center">
           <FaArrowLeft className="text-lg mr-3" /> Back
         </button>
       </Link>
       {/* email subject here */}
-      <section className="flex justify-between mb-5">
+      <section className="lg:flex justify-between mb-5">
         <div className="flex items-center gap-5 flex-wrap mb-5">
           <h1 title="Subject" className="text-2xl font-semibold ">
-            {emailData[parseInt(id) - 1]?.subject}
+            {currentData?.subject}
           </h1>
           <div className="badge badge-accent gap-2">
-            {emailData[parseInt(id) - 1]?.label}
+            {currentData?.label}
             <GiCrossedBones></GiCrossedBones>
           </div>
         </div>
         <div className="flex items-center gap-5 flex-wrap">
-          <button className="btn">Download</button>
-          <button className="btn ">Print</button>
-          <button className="btn">Delete</button>
+          <button className="btn btn-sm lg:btn-md">Download</button>
+          <button className="btn btn-sm lg:btn-md">Print</button>
+          <button className="btn btn-sm lg:btn-md">Delete</button>
         </div>
       </section>
 
       {/* email description here  */}
       <section className="bg-base-200 p-5 rounded-md min-h-[calc(100vh-160px)] ">
-        <div className="flex items-center justify-between mb-5">
+        <div className="lg:flex items-center justify-between mb-5">
           <div className="flex items-center">
             <img
               src="https://i.pravatar.cc/500?img=32"
@@ -50,9 +51,9 @@ const InboxDetails = () => {
             />
             <div>
               {/* user info */}
-              <div className="flex">
-                <p className="mr-3">{emailData[parseInt(id) - 1]?.name}</p>
-                <p className="">{emailData[parseInt(id) - 1]?.email}</p>
+              <div className="lg:flex">
+                <p className="lg:mr-3 font-bold">{currentData?.name}</p>
+                <p className="font-medium">{currentData?.email}</p>
               </div>
 
               {/* sender mail  */}
@@ -60,17 +61,17 @@ const InboxDetails = () => {
                 <CiMail className="text-lg" />
                 {emailData[1]?.email}
               </p>
+              <p className="">{currentData?.time}</p>
             </div>
-          </div>
-
-          {/* date */}
-          <div className="text-end">
-            <p className="">{emailData[parseInt(id) - 1]?.time}</p>
+            {/* date */}
+            {/* <div className="lg:text-end">
+              <p className="">{currentData?.time}</p>
+            </div> */}
           </div>
 
           {/* email info */}
         </div>
-        <p className="px-20">{emailData[parseInt(id) - 1]?.description}</p>
+        <p className="lg:px-5 xl:px-20">{currentData?.description}</p>
       </section>
     </div>
   );
