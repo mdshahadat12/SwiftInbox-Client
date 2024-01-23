77b3d9346c7b4e5e9f1db9141603f06f9b3c9679
akd420 <ayanakd112@gmail.com>
2024-01-23 23:22:52 +0600

    added modals for refresh and delete

--- src/Components/EmailBox.jsx
+++ src/Components/EmailBox.jsx
@@ -1,8 +1,24 @@
 import { IoIosRefresh } from "react-icons/io";
 import { CiEdit } from "react-icons/ci";
 import { AiOutlineDelete } from "react-icons/ai";
+import toast from "react-hot-toast";
 const EmailBox = () => {
   const userEmail = "example@tempmail.com"; // change this to the temp email we get from the website
+
+  //add refetch function here
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  //add change email function here
+  const handleChangeEmail = () => {
+    toast.success("Email changed successfully");
+  };
+
+  //add delete function here
+  const handleDelete = () => {
+    toast.success("Email deleted");
+  };
   return (
     <div className="max-w-screen-xl mx-auto my-12 px-4">
       <div className="flex items-center justify-center my-8">
@@ -15,16 +31,26 @@ const EmailBox = () => {
               <h3 className="text-center font-semibold text-lg">{userEmail}</h3>
             </div>
             <div className="flex items-center justify-around gap-2">
-              <button className="btn btn-accent">
+              <button onClick={handleRefresh} className="btn btn-accent">
                 {" "}
                 <IoIosRefresh />
                 Refresh
               </button>
-              <button className="btn btn-accent">
+              <button
+                onClick={() =>
+                  document.getElementById("changeModal").showModal()
+                }
+                className="btn btn-accent"
+              >
                 {" "}
                 <CiEdit /> Change
               </button>
-              <button className="btn btn-accent">
+              <button
+                onClick={() =>
+                  document.getElementById("deleteModal").showModal()
+                }
+                className="btn btn-accent"
+              >
                 {" "}
                 <AiOutlineDelete /> Delete
               </button>
@@ -32,6 +58,63 @@ const EmailBox = () => {
           </div>
         </div>
       </div>
+      {/* delete modal here  */}
+      <dialog id="deleteModal" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Are you sure you want to delete this temporary email address?
+          </h3>
+          <p className="py-4">
+            Deleting this email address will also delete all the messages in the
+            inbox.
+          </p>
+          <div className="">
+            <form
+              method="dialog"
+              className="flex items-center justify-center gap-6"
+            >
+              <button
+                onClick={handleDelete}
+                className="btn bg-red-600 text-white"
+              >
+                Confirm
+              </button>
+              <button className="btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+      {/* change email modal here  */}
+      <dialog id="changeModal" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <div className="text-center">
+            <input
+              type="text"
+              placeholder="New Email address...."
+              className="input input-bordered input-accent text-center w-full max-w-xs"
+              defaultValue={userEmail}
+            />
+          </div>
+          <p className="py-4">
+            Changing this email address will also delete all the messages in the
+            inbox.
+          </p>
+          <div>
+            <form
+              method="dialog"
+              className="flex items-center justify-center gap-6"
+            >
+              <button
+                onClick={handleChangeEmail}
+                className="btn bg-accent text-white"
+              >
+                Change
+              </button>
+              <button className="btn">Cancel</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
