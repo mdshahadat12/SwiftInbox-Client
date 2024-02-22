1c4af4f462e9645e8f5d5fe4c28890b3c0875482
Alien Brain <murad.dub@gmail.com>
2024-02-23 00:43:04 +0600

    footer & nav updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,7 @@
 const Footer = () => {
-  const containerStyle = {
-    clipPath: "polygon(0 60%, 100% 36%, 100% 100%, 0% 100%)",
-  };
-
   return (
-    <div
-      className="z-50 w-full p-10 bg-blue-700/15 flex justify-center items-center align-middle"
-      style={containerStyle}
-    >
-      <p className="text-white text-xs font-semibold mt-16">
+    <div className="z-50 w-full p-10 bg-blue-700/15 flex justify-center items-center align-middle">
+      <p className="text-white text-xs font-semibold">
         Copyright © 2024 - All right reserved by Team CodeCrafters
       </p>
     </div>
