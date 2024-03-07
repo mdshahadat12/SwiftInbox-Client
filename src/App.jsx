e4608b7b01345a9eb3aebb23f303388ef6c3e1e8
antubhatta <antubhatta09@gmail.com>
2024-03-07 13:05:36 +0600

    why choose us section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet";
 import EmailBox from "./Components/EmailBox";
 import Inbox from "./Page/Inbox";
 import Faq from "./Page/Faq";
+import WhyChooseUs from "./Page/WhyChooseUs";
 function App() {
   return (
     <>
@@ -13,8 +14,13 @@ function App() {
         <hr />
       </div>
       <Inbox></Inbox>
+      <div className="max-w-screen-xl mx-auto">
+        <hr />
+      </div>
+      <WhyChooseUs></WhyChooseUs>
       <hr />
       <Faq></Faq>
+    
     </>
   );
 }
